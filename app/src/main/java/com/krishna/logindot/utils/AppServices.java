b200b34deb3f4c87986f976753ec9ae65cc8ef1b
Krishnamoorthy R (ant000150) <krishnamoorthy.raju@seaant.com>
2018-05-18 17:42:07 +0530

    appservice modified

--- app/src/main/java/com/krishna/logindot/utils/AppServices.java
+++ app/src/main/java/com/krishna/logindot/utils/AppServices.java
@@ -4,18 +4,23 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,6 +38,8 @@ public class AppServices {
         String loginResponse = "";
         String url = AppConstants.loginUrl;
         String  REQUEST_TAG = "loginRequest";
+        String device_id = AppConstants.device_id;
+        int device_type = AppConstants.device_type;
 
         RequestQueue queue = VolleySingleton.getInstance(context).getRequestQueue();
 
@@ -46,7 +53,23 @@ public class AppServices {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
-                callback.onResponse(error.toString());
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data,
+                                HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        // Now you can use any deserializer to make sense of data
+                        JSONObject obj = new JSONObject(res);
+                        callback.onResponse(String.valueOf(obj));
+                    } catch (UnsupportedEncodingException e1) {
+                        // Couldn't properly decode data to string
+                        e1.printStackTrace();
+                    } catch (JSONException e2) {
+                        // returned data is not JSONObject?
+                        e2.printStackTrace();
+                    }
+                }
+                //callback.onResponse(error.toString());
             }
         })
         {
@@ -57,12 +80,13 @@ public class AppServices {
                 //params.put("Authorization: Basic", TOKEN);
                 params.put("email", email);
                 params.put("password", password);
-                params.put("device_id", device_id);
-                params.put("device_type", String.valueOf(device_type));
+                params.put("device_id", "b4191ec717ee49f6");
+                params.put("device_type", "1");
                 return params;
             }
         };
         VolleySingleton.getInstance(context).addToRequestQueue(strReq, REQUEST_TAG);*/
+
 
         JSONObject jsonObject = new JSONObject();
         try {
@@ -74,8 +98,16 @@ public class AppServices {
             e.printStackTrace();
         }
 
+        /*Map< String, String > params = new HashMap< String, String >();
+        //params.put("Authorization: Basic", TOKEN);
+        params.put("email", email);
+        params.put("password", password);
+        params.put("device_type", String.valueOf(device_type));
+        params.put("device_id", device_id);
+        JSONObject jsonObject = new JSONObject(params);*/
+
         Log.e("jsonObject ", String.valueOf(jsonObject));
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(POST, url, jsonObject, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 callback.onResponse(String.valueOf(response));
@@ -83,9 +115,35 @@ public class AppServices {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                callback.onResponse(String.valueOf(error));
+
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data,
+                                HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        // Now you can use any deserializer to make sense of data
+                        JSONObject obj = new JSONObject(res);
+                        callback.onResponse(String.valueOf(obj));
+                    } catch (UnsupportedEncodingException e1) {
+                        // Couldn't properly decode data to string
+                        e1.printStackTrace();
+                    } catch (JSONException e2) {
+                        // returned data is not JSONObject?
+                        e2.printStackTrace();
+                    }
+                }
+
             }
-        });
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                HashMap<String,String> headers = new HashMap();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+        };
         VolleySingleton.getInstance(context).addToRequestQueue(jsonObjectRequest, REQUEST_TAG);
     }
 
